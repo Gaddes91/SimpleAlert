bd6675b3574cf3138a1c685aab78f713ee236243
Matthew Gaddes <matthew.gaddes@gmail.com>
2015-11-07 11:52:10 +0000

    Initial Commit

---
+++ SimpleAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleAlert
+//
+//  Created by Matthew Gaddes on 07/11/2015.
+//  Copyright © 2015 Matthew Gaddes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
